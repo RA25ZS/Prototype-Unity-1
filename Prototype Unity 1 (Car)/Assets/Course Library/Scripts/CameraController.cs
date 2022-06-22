bbf4809ca23f7b0d396809c6cd31b2c18bcf3bdd
Andrey Ryshkevich <paulbegitson@mail.ru>
2022-06-23 00:21:19 +0300

    Fix Camera with LateUpdate

--- Prototype Unity 1 (Car)/Assets/Course Library/Scripts/CameraController.cs
+++ Prototype Unity 1 (Car)/Assets/Course Library/Scripts/CameraController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] GameObject player;
+    Vector3 cameraOffest = new Vector3(-0.11f, 4.2f, -5.3f);
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +13,8 @@ public class CameraController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = player.transform.position + new Vector3(-0.11f, 4.2f, -5.3f);
+        transform.position = player.transform.position + cameraOffest;
     }
 }
--- Prototype Unity 1 (Car)/Assets/Course Library/Scripts/PlayerMovement.cs
+++ Prototype Unity 1 (Car)/Assets/Course Library/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float carSpeed = 2.5f;
+    float carSpeed = 5.5f;
 
      void Start()
     {
